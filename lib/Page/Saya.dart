09738534e2bebc226f01e9ee6dba2ab6e15dccc2
Daniel P <rafisenpai@gmail.com>
2023-06-14 00:17:51 +0700

    update page Saya.dart untuk test

--- lib/Page/Saya.dart
+++ lib/Page/Saya.dart
@@ -1,26 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SayaPage extends StatelessWidget {
-  const SayaPage({Key? key}) : super(key: key);
+class SayaPage extends StatefulWidget {
+  const SayaPage({super.key});
 
   @override
+  State<SayaPage> createState() => _SayaPageState();
+}
+
+class _SayaPageState extends State<SayaPage> {
+  @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          pinned: true,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Container(
-              color: Color(0xFF70986c),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+              leading: const Icon(Icons.add_a_photo_rounded),
+              title: const Text("Nama")),
+          SliverToBoxAdapter(
+            child: Container(
+              child:
+              const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(children: [
+                    Icon(Icons.list), Text("Daftar Ekspedisi"),],),
+                  Row(children: [
+                    Icon(Icons.delete), Text("Hapus Riwayat Cek Resi"),],),
+                  Row(children: [
+                    Icon(Icons.share), Text("Bagikan"),],),
+                  Row(children: [
+                    Icon(Icons.mail), Text("Kontak Kami"),],),
+                  Row(children: [
+                    Icon(Icons.exit_to_app), Text("Keluar"),],),
+                ],),
             ),
-          ),),
-        SliverList(
-            delegate: SliverChildListDelegate([
-              Text("Nama"),
-              Text("+6281234567891"),
-            ]),),
-      ],),
-  );}
+          ),],
+      ),);
+  }
 }
