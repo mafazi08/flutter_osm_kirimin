fb7b173c53a0a972ad20a15ed69249ed0ed309da
Daffa <daffamafazi8@gmail.com>
2023-06-13 07:10:34 +0700

    Revert "update nama dan logo aplikasi"
    This reverts commit 042cb6d185b974dbec3aaf8573bedb688534b97f.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_osm_kirimin/Page/Beranda.dart';
 import 'package:flutter_osm_kirimin/Page/Daftar_Paket.dart';
 import 'package:flutter_osm_kirimin/Page/Saya.dart';
 
+
 void main() {
   runApp(
     ChangeNotifierProvider(
