6926c599e6b60f7847be5fec0da6e25125305c9a
Mafazi <Mafazi@LAPTOPKU>
2023-06-14 10:41:33 +0700

    Revisi Saya.dart

--- lib/Page/Saya.dart
+++ lib/Page/Saya.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SayaPage extends StatefulWidget {
   const SayaPage({super.key});
@@ -15,27 +14,85 @@ class _SayaPageState extends State<SayaPage> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar.large(
-              leading: const Icon(Icons.add_a_photo_rounded),
-              title: const Text("Nama")),
+            // Set the pinned property to true
+            pinned: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Icon(Icons.person, size: 60, color: Colors.white,),
+                Column(
+                  children: [
+                    // Set the style property of the Text widget
+                    const Text(
+                      "Nama",
+                      style: TextStyle(fontSize: 20, color: Colors.white), // Set the font size to 20
+                    ),
+                    // Set the style property of the Text widget
+                    const Text(
+                      "0123456789", // Replace with the desired handphone number
+                      style: TextStyle(fontSize: 20, color : Colors.white), // Set the font size to 20
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            backgroundColor: const Color(0xFF70986C),
+          ),
           SliverToBoxAdapter(
             child: Container(
-              child:
-              const Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(children: [
-                    Icon(Icons.list), Text("Daftar Ekspedisi"),],),
-                  Row(children: [
-                    Icon(Icons.delete), Text("Hapus Riwayat Cek Resi"),],),
-                  Row(children: [
-                    Icon(Icons.share), Text("Bagikan"),],),
-                  Row(children: [
-                    Icon(Icons.mail), Text("Kontak Kami"),],),
-                  Row(children: [
-                    Icon(Icons.exit_to_app), Text("Keluar"),],),
-                ],),
+                  ListTile(
+                    leading: Icon(Icons.list),
+                    title: Text("Daftar Ekspedisi"),
+                    onTap: () {
+                      // Handle button press
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.delete),
+                    title: Text("Hapus Riwayat Cek Resi"),
+                    onTap: () {
+                      // Handle button press
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.share),
+                    title: Text("Bagikan"),
+                    onTap: () {
+                      // Handle button press
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.mail),
+                    title: Text("Kontak Kami"),
+                    onTap: () {
+                      // Handle button press
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      icon: Icon(Icons.exit_to_app),
+                      label: Text("Keluar", style: TextStyle(fontSize: 20, color: Colors.white),),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF70986C),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                          )
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),],
+          ),
+        ],
       ),);
   }
 }
